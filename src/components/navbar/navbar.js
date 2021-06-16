00d2448d0f2e7cb1f6c624801f60f52313691580
GonzaloEspinoza <gonis1990@gmail.com>
2021-06-16 12:31:02 -0400

    secont test

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,13 +9,13 @@ const Navbar =()=>{
                     <div className="my-auto">
                         <h1 className="font-bold mx-10 text-xl text-green-500 ">NuxtBo</h1>
                     </div>
-                    <div className="hidden sm:flex my-auto flex flex-row">
+                    {/* <div className="hidden sm:flex my-auto flex flex-row">
                         <li>1</li>
                         <li>2</li>
                         <li>333</li>
                         <li>4444</li>
                         <li>5555</li>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="flex  mr-2 md:mr-16">
                     <div className="my-auto p-2 px-3 bg-green-400 text-white font-semibold rounded-xl cursor-pointer">
